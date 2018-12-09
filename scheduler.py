5d4dbae7fd47a13b2b1db2d9952d3960d5ee0e9b
willem <willem@willems-MacBook-Pro.local>
2018-12-09 13:39:33 +0000

    progress

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,6 @@
 import argparse
 import re
+import random
 
 parser = argparse.ArgumentParser(description=
                                  """
@@ -19,11 +20,11 @@ class Scheduler():
         activities = {}
 
         for line in content:
-            components = re.split('\s(\w+)$', line) #get the last term, e.g. '60min'
+            components = re.split('\s(\w+)$', line)  # get the last term, e.g. '60min'
             if components[1] == 'sprint':
                 activities[components[0]] = 15
             else:
-                activities[components[0]] = int(re.findall(r'\d+',components[1])[0]) #get the numbers only
+                activities[components[0]] = int(re.findall(r'\d+',components[1])[0])  # get the numbers only
 
         return activities
 
@@ -36,17 +37,27 @@ class Scheduler():
             schedules.append([])
 
         for schedule in schedules:
-            schedule.append(activities.popitem())
+            schedule.append(random.choice(activities.items()))
 
-        for schedule in schedules:
-            activity_pool = activities
+        for index, schedule in enumerate(schedules):
             while sum([event[1] for event in schedule]) < 420:
-                try:
-                    new_item = activity_pool.popitem()
-                    if new_item not in schedule:
-                        schedule.append(new_item)
-                except KeyError as e:
-                    break
+                act = random.choice(activities.items())
+                if act not in schedule:
+                    for s in schedules[index:] + schedules[:index + 1]:
+                        if sum([event[1] for event in schedule]) > sum([event[1] for event in s]):
+                            schedule.append(act)
+                        elif s[-1] != act and s[-1][1] >= act[1]:
+                            schedule.append(act)
+
+        # for schedule in schedules:
+        #     activity_pool = activities
+        #     while sum([event[1] for event in schedule]) < 420:
+        #         try:
+        #             new_item = activity_pool.popitem()
+        #             if new_item not in schedule:
+        #                 schedule.append(new_item)
+        #         except KeyError as e:
+        #             break
 
         # while sum([event[1] for event in s for s in schedules]) < 420:
         #     if 135 < sum([event[1] for event in schedule for schedule in schedules]) < 180:
@@ -59,6 +70,6 @@ class Scheduler():
 
 a = Scheduler()
 
-print a.file_parser('activities.txt')
+a.file_parser('activities.txt')
 
 
