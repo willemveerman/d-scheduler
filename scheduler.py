ef6c13a35c619fae67ea416c9bb42b4712e6378c
Willem Veerman <willemveerman@gmail.com>
2018-12-10 08:23:44 +0000

    progress

--- scheduler.py
+++ scheduler.py
@@ -10,19 +10,31 @@ parser = argparse.ArgumentParser(description=
                                  and produces an activity schedule 
                                  for said number of teams.
                                  """)
+
+parser.add_argument('-i', '--input',
+                    help='path to the input file')
+
+parser.add_argument('-r', '--region',
+                    default='eu-west-2',
+                    help='AWS region')
+
+parser.add_argument('-n', '--name',
+                    default='*',
+                    help='EC2 instance name')
+
 class Schedule():
-    """A class for storing a schedule for a single team"""
+    """A schedule for a single team"""
     def __init__(self):
         self.events = []
 
     def total(self):
-        """Returns the total duration of all activities in the object"""
+        """The total duration of all activities in the schedule"""
         return sum([event[1] for event in self.events])
 
 class Scheduler():
 
     def file_parser(self, path):
-        """Parse a file and return a dict of str(activity):int(duration)"""
+        """Returns a dict of str(activity):int(duration)"""
         with open(path) as f:
             content = f.readlines()
 
@@ -55,23 +67,23 @@ class Scheduler():
         return events
 
     def scheduler(self, teams, activities):
-        """Given n teams and a list of activites and their durations, produce a schedule
+        """Given n teams and a dict of activites and their durations, produce a schedule
            for each team that does not overlap with that of any other team"""
         if teams > 13:
             return "You cannot divide into more than 13 teams without overlaps (if you want to avoid gaps)"
 
         act_copy = activities.items()
 
-        schedules = []
-
         lunch = ('Lunch Break', 60)
+
+        schedules = []
 
         for _ in range(teams):
             schedules.append(Schedule())
 
         for index, schedule in enumerate(schedules):
             while schedule.total() < 420:  # arbitrary
-                if schedule.total() > 150 and lunch not in schedule.events:  # arbitrary
+                if schedule.total() > 120 and lunch not in schedule.events:  # arbitrary
                     schedule.events.append(lunch)
                 try:
                     event = activities.popitem()  # ensure that every activity is tried at least once
@@ -99,7 +111,7 @@ class Scheduler():
 
 a = Scheduler()
 
-teams = 14
+teams = 6
 
 b = a.scheduler(teams, a.file_parser('activities.txt'))
 
